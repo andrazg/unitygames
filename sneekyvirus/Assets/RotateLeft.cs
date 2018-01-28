2fe2de77cbc4bbb49d832fbeed747bea81f47dae
andrazg <andraz.gruden@gmail.com>
2018-01-28 15:56:15 +0100

    game

---
+++ sneekyvirus/Assets/RotateLeft.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateLeft : MonoBehaviour {
+
+	public float rotateSpeed = 200f;
+	public float moveSpeed = 1f;
+
+	void Update(){
+		transform.Rotate (Vector3.up * Mathf.Sin(Time.deltaTime) * rotateSpeed, Space.World);
+    }
+}
---
+++ sneekyvirus/Assets/Rotator.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour {
+
+	public float rotateSpeed = 200f;
+	public float moveSpeed = 1f;
+
+	void Update(){
+		transform.Rotate (Vector3.up * Time.deltaTime * rotateSpeed, Space.World);
+		transform.Translate (Vector3.up * Time.deltaTime * moveSpeed, Space.World);
+	}
+}
