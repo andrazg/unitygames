d68b9340aaa1774decc6ed67c8e397d5b1b3aad8
grudwa <andraz.gruden@gmail.com>
2018-05-08 10:29:23 +0200

    proxy love

---
+++ proxiMem/Assets/Love/Scripts/BlendKeyNet.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+public class BlendKeyNet : MonoBehaviour {
+
+
+    float changePercent = 0;
+    bool turn = true;
+
+    SkinnedMeshRenderer skin;
+	void Start () {
+        skin = GetComponent<SkinnedMeshRenderer>();
+       InvokeRepeating("ChangerSkin", 0, 0.003f);
+	}
+
+    void ChangerSkin() {
+
+        if (turn) {
+            changePercent += 3;
+            if (skin.GetBlendShapeWeight(0) < 100)
+            {
+                skin.SetBlendShapeWeight(0, changePercent);
+            }
+            if (skin.GetBlendShapeWeight(0) >= 100) {
+                turn = !turn;
+            }
+        }
+
+        if (!turn) {
+            changePercent -= 3;
+            if (skin.GetBlendShapeWeight(0) > 0)
+            {
+                skin.SetBlendShapeWeight(0, changePercent);
+            }
+            if (skin.GetBlendShapeWeight(0) <= 0)
+            {
+                turn = !turn;
+            }
+        }
+
+    }
+}
---
+++ proxiMem/Assets/Love/Scripts/RotateAround.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateAround : MonoBehaviour
+{
+
+    public Transform rotate;
+    public float speed = 10;
+
+    void Update()
+    {
+        transform.RotateAround(rotate.position, Vector3.up, speed * Time.deltaTime);
+    }
+}
