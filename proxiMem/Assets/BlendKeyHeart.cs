af03c31dfbf88e84f1922cbf5bb1dab7a52391bf
andrazg <andraz.gruden@gmail.com>
2018-05-08 23:12:14 +0200

    proxi mem love

---
+++ proxiMem/Assets/BlendKeyHeart.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class BlendKeyHeart : MonoBehaviour {
+
+    public float speedrotation = 10;
+    float changepercent = 0;
+    SkinnedMeshRenderer skin;
+
+	void Start () {
+        skin = GetComponent<SkinnedMeshRenderer>();
+	}
+
+	void Update () {
+        transform.RotateAround(transform.position, Vector3.up, speedrotation * Time.deltaTime);
+
+
+        if (skin.GetBlendShapeWeight(0) < 100) {
+            changepercent++;
+            skin.SetBlendShapeWeight(0, changepercent/2);
+        }
+	}
+}
