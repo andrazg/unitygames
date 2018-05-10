aea9ecbd359faa9add4b6ff80d0b1f5a7694f7e4
grudwa <andraz.gruden@gmail.com>
2018-05-10 16:09:11 +0200

    end proxy love

---
+++ proxiMem/Assets/Reset.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Reset : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        SceneManager.LoadScene("love");
+	}
+	
+
+}
